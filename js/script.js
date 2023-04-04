eb1c00d4a127110af6e361950f89a2fc55f559de
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-04-04 16:35:32 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates the volume of a pyramid
+ */
+"use strict"
+
+function calculate() {
+  // input
+  const length = parseInt(document.getElementById("length").value)
+  const width = parseInt(document.getElementById("width").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const volume = length * width * height / 3 
+
+  // output
+  document.getElementById("volume").innerHTML = "The volume is: " + volume + " cm³"
+}
